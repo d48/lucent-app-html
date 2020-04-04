afaac229b043a4a7d1e0ed70c05a1b1796281569
Johnr302 <johnathon.regalado@gmail.com>
2020-04-04 14:52:56 -0700

    toggle menu function correctly

--- hambMenuBtn.js
+++ hambMenuBtn.js
@@ -1,9 +1,11 @@
 const el = document.getElementById("hamburger-menu");
+const menuElement = document.getElementById("client-menu");
 
 let isActive = false;
 
 const toggleMenu = (event) => {
   el.classList.toggle("is-active");
+  menuElement.classList.toggle("change");
 };
 
 el.addEventListener("click", toggleMenu);
